9dc8b70a7f9dc6da3ff4bca2bd78ee0a2e8e4966
yathk <ykid285@aucklanduni.ac.nz>
2023-01-26 00:44:01 +1300

    before big change

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { Editor } from '@tinymce/tinymce-react';
+import { v4 as uuidv4 } from 'uuid';
 
 import './index.css';
 import './CSS/sidebar.css';
@@ -8,13 +9,189 @@ import './CSS/navbar.css';
 import SideBar from './components/sidebar_components';
 import { Navbar, NavItem } from './components/navbar_components';
 import { QDetails } from './components/qDetails_components';
+import { Choice } from './components/variable_class.js'
 
 const VAR_DECORATOR = '@';
-const ENTERED_VARS = [];
+const SAVED_VARIABLES = [];
+const ENTERED_VARIABLES = [];
+
+SAVED_VARIABLES.push(new Choice('myFirstVariable'))
 
 
-function QuestionEditor(initialValue) {
-  
+
+const handleVariableEntered = (enteredVariable) => {
+  let found = false;
+  for (let var_index in SAVED_VARIABLES) {
+    if (SAVED_VARIABLES[var_index].title === enteredVariable.name) {
+      console.log('VARIABLE FOUND AT ' + var_index);
+      found = true;
+      break;
+    }
+  }
+  if (!found) {
+    const newVar = new Choice(enteredVariable.name, true);
+    newVar.addInstance(parseInt(enteredVariable.instanceId));
+    SAVED_VARIABLES.push(newVar);
+  }
+}
+
+function delay(time) {
+  return new Promise(resolve => setTimeout(resolve, time));
+}
+
+
+
+// const validateVariableName = (varName) => {
+//   let output = varName.replaceAll('@', '');
+//   output = output.replaceAll(' ', '');
+//   return output;
+// }
+
+
+// const validateVariables = (htmlBody) => {
+//   console.log(htmlBody.getGetElementsByClassName('variable'))
+// }
+
+
+
+function QuestionEditor({ initialValue }) {
+
+  const [value, setValue] = useState(initialValue ?? '');
+  useEffect(() => setValue(initialValue ?? ''), [initialValue]);
+
+  // const isEnteringVar = useRef(false);
+  // const varEntered = useRef('');
+  const varInstanceCount = useRef(0);
+  const editorContent = useRef('');
+
+  // const getNextInstanceId = () => {
+  //   varInstanceCount.current += 1;
+  //   return varInstanceCount.current;
+  // }
+
+  // check all vars and update internal state
+  const parseVars = (contentHtml) => {
+    console.log(contentHtml.activeElement)
+    const variablesHtml = Array.from(contentHtml.activeElement.getElementsByClassName('variable'));
+    SAVED_VARIABLES.forEach((sv) => {
+      sv.instances.forEach((inst) => {
+        // remove instance if deleted from editor, 
+        if (!contentHtml.activeElement.getElementById('' + inst)) {
+          sv.removeInstance(inst)
+        }
+      })
+    })
+    
+    variablesHtml.forEach((varHtml) => {
+      if ( !varHtml.getAttribute('id')) {
+        // add unique 'id' to span wrapper
+        const varId = varInstanceCount.current
+        varInstanceCount.current++;
+        varHtml.setAttribute('id', varId);
+        let isFoundSavedVariable = false;
+        // add new instance if variable saved
+        for (let savedVar of SAVED_VARIABLES) {
+          if (savedVar.title === varHtml.innerText) {
+            savedVar.addInstance(varId);
+            isFoundSavedVariable = true;
+            break;
+          }
+        }
+        // save variable if its new
+        if ( !isFoundSavedVariable ) {
+          const newVar = new Choice(varHtml.innerText)
+          newVar.addInstance(varId);
+          SAVED_VARIABLES.push(newVar)
+        }
+      }
+    });
+
+    // console.log(SAVED_VARIABLES);
+  }
+
+  return (
+    <>
+      <Editor
+        initialValue={initialValue}
+        value={value}
+        onEditorChange={(newValue, editor) => {
+          setValue(newValue);
+        }}
+
+        tinymceScriptSrc={process.env.PUBLIC_URL + '/tinymce/tinymce.min.js'}
+        init={{
+          height: 500,
+          menubar: false,
+          browser_spellcheck: true,
+          contextmenu: false,
+          formats: {
+            'custom-variable-style': {
+              inline: 'span',
+              wrapper: true,
+              styles: {
+                'color': '#c2c2c2',
+                'font-weight': 'bold'
+              },
+              attributes: {
+                // 'id': '' + varInstanceCount.current,
+                'class': 'variable',
+                'contenteditable': 'false',
+              }
+            }
+          },
+          setup: (editor) => {
+
+
+            // parse variables on every keyboard input
+            editor.on('input', (e) => {
+              console.log('fired');
+              delay(500).then(() => {
+                const domVars = editor.dom.select('span.variable');
+                const varIds = [];
+                domVars.forEach( (v) => {
+                  const id = v.getAttribute('id');
+                  if(id && !varIds.includes(id)) {
+                    varIds.push(id);
+                  }
+                  if (!id || varIds.includes(id))
+                    v.setAttribute('id', '' + varInstanceCount.current++);
+                } )
+              })
+
+              editorContent.content = editor.getDoc();
+              // parseVars(editorContent.content);
+              // editor.setContent()
+              // console.log(editorContent.current)
+            });
+
+            // parse on text pattern match
+            editor.on('FormatApply', (e) => {
+              if (e.format === 'custom-variable-style') {
+                editorContent.current = editor.getDoc();
+                // parseVars(editorContent.current);
+              }
+            })
+
+          },
+          text_patterns: [
+            { start: VAR_DECORATOR, end: VAR_DECORATOR, format: 'custom-variable-style' },
+          ],
+          plugins: [
+            'advlist', 'autolink', 'lists', 'link', 'image', 'charmap',
+            'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
+            'insertdatetime', 'media', 'table', 'preview', 'help', 'wordcount'
+          ],
+          toolbar: 'undo redo | blocks | fontsize | ' +
+            'bold italic underline forecolor | alignleft aligncenter ' +
+            'alignright alignjustify | bullist numlist outdent indent | ' +
+            'removeformat | help',
+          content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14pt }'
+        }}
+      />
+      <button onClick={() => console.log(value)}>Log editor content</button>
+    </>
+
+  )
 }
 
 
@@ -28,15 +205,7 @@ function QuestionEditor(initialValue) {
 
 
 function App() {
-  const editorRef = useRef(null);
-  const [varEntered, setVarEntered] = useState(null);
-  const [isEnteringVar, setIsEnteringVar] = useState(false);
 
-  const log = () => {
-    if (editorRef.current) {
-      console.log(editorRef.current.getContent());
-    }
-  };
 
   return (
     <div className="App">
@@ -51,73 +220,11 @@ function App() {
         </div>
 
         <div className='tinyEditor'>
-          <>
-            <Editor
-              tinymceScriptSrc={process.env.PUBLIC_URL + '/tinymce/tinymce.min.js'}
-              onInit={(evt, editor) => editorRef.current = editor}
-              initialValue='<p>This is the initial content of the editor.</p>'
-              init={{
-                height: 500,
-                menubar: false,
-                browser_spellcheck: true,
-                contextmenu: false,
-                setup: (editor) => {
-                  editor.addCommand('makevariable', (ui, v) => {
-                    console.log(v);
-                  });
-
-                  editor.on('input', (e) => {
-                    // console.log(e.data)
-
-
-
-                    if (e.data === VAR_DECORATOR) {
-                      // console.log(`SETTING isEnteringVar TO ${!isEnteringVar}`)
-
-                      console.log(`STATE is now ${isEnteringVar}`);
-                      
-                      if (isEnteringVar) {
-                        setIsEnteringVar(false);
-                      } else {
-                        setIsEnteringVar(true);
-                      }
-
-                      console.log(`STATE is now ${isEnteringVar}`);
-
-
-                      // setVarEntered('');
-
-                      // if (!isEnteringVar && varEntered) {
-                      //   console.log(`entered variable ${varEntered}`);
-                      //   ENTERED_VARS.push(varEntered);
-                      //   setVarEntered(null);
-                      // } else if (isEnteringVar && varEntered !== null) {
-                      //   console.log(`SETTING varEntered TO ${varEntered + e.data}`)
-                      //   setVarEntered(varEntered + e.data);
-                      // }
-
-                    }
-
-                  });
-                },
-                // text_patterns: [
-                //   {start:VAR_DECORATOR, end:VAR_DECORATOR, cmd: 'makevariable', value: 'hello'}
-                // ],
-                plugins: [
-                  'advlist', 'autolink', 'lists', 'link', 'image', 'charmap',
-                  'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
-                  'insertdatetime', 'media', 'table', 'preview', 'help', 'wordcount'
-                ],
-                toolbar: 'undo redo | blocks | fontsize | ' +
-                  'bold italic forecolor | alignleft aligncenter ' +
-                  'alignright alignjustify | bullist numlist outdent indent | ' +
-                  'removeformat | help',
-                content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
-              }}
-            />
-            <button onClick={log}>Log editor content</button>
-          </>
+          <QuestionEditor
+            initalValue='hello'
+          />
         </div>
+
       </div>
     </div>
   );
@@ -131,8 +238,51 @@ export default App;
 const highlightVariables = (content) => {
   const regexp = /(?!\\)@[0-9]+/g;
   const matches = [...content.matchAll(regexp)].reverse();
-  matches.forEach( match => {
+  matches.forEach(match => {
     content = content.replace(match, `<span style='color:red;>${match}</span>`)
   })
   return content;
 };
+
+
+
+
+
+
+
+              // console.log(varInstanceCount.count)
+              // varInstanceCount.current++;
+
+
+              // editor.setContent(contentHtml.activeElement.innerHTML)
+
+
+
+              //   if (e.data === VAR_DECORATOR) {
+              //     if (!isEnteringVar.current) {
+              //       isEnteringVar.current = true;
+              //     }           
+              //   }
+
+              //   if (e.data === ' ') {
+              //     const lastLetterI = varEntered.current.length-1;
+              //     if (varEntered.current.slice(lastLetterI) === '@') {
+              //       isEnteringVar.current = false;
+              //     }
+              //   }
+
+              //   if (isEnteringVar.current && e.inputType === 'insertText') {
+              //       varEntered.current += e.data;
+              //   } else if (!isEnteringVar.current && varEntered.current.length > 0) {
+
+              //       const enteredVar = {
+              //         'name': validateVariableName(varEntered.current),
+              //         'instanceId': '' + varInstanceCount.current
+              //         }
+              //       handleVariableEntered(enteredVar);
+
+              //       varEntered.current = '';
+              //       varInstanceCount.current++;
+
+              //       console.log(SAVED_VARIABLES);
+              //   }
---
+++ src/components/variable_class.js
@@ -0,0 +1,82 @@
+class Variable {
+    #title;
+    #instances;
+
+    constructor(title, isInEditor=false) {
+      this.#title = title;
+      this.#instances = [];
+    }
+
+    get title() {
+      return this.#title;
+    }
+
+    set title(newTitle) {
+      if ( ! (typeof newTitle === 'string' || newTitle instanceof String) ) {
+        console.log("ERROR: Variable title must be a string!");
+      } else if (newTitle.includes(' ')) {
+        console.log("ERROR: Variable names can't have spaces!")
+      } else {
+        this.#title = newTitle;
+      }
+    }
+
+    get instances() {
+      return [...this.#instances];
+    }
+
+    removeInstance = (instanceId) => {
+      const index = this.#instances.indexOf(instanceId);
+      if (index > -1) {
+        this.#instances.splice(index, index);
+      } else {
+        console.log("ERROR: instanceId not found in variable.")
+      }
+    }
+
+    addInstance = (instanceId) => {
+      if ( ! Number.isInteger(instanceId) ){
+        console.log('ERROR: instanceID must be an integer');
+      } else if (this.#instances.includes(instanceId)) {
+        console.log('ERROR: instaceID already exists.');
+      } else {
+        this.#instances.push(instanceId);
+      }
+    }
+
+    isInstance = (instanceId) => {
+      return this.#instances.includes(instanceId);
+    }
+
+    isInEditor = () => {
+      return !(this.#instances.length === 0);
+    }
+}
+
+export class Choice extends Variable {
+  #choices;
+
+  constructor(title, isInEditor) {
+    super(title, isInEditor);
+    this.#choices = [];
+  }
+
+  addChoice = (choiceToAdd) => {
+    if ( ! (typeof choiceToAdd === 'string' || choiceToAdd instanceof String) ) {
+      console.log("ERROR: Choice value must be a string!");
+    } else if ( this.#choices.includes(choiceToAdd) ) {
+      console.log("ERROR: Choice value already exists!");
+    } else {
+      this.#choices.add(choiceToAdd);
+    }
+  }
+
+  removeChoice = (choiceToRemove) => {
+    if ( ! this.#choices.includes(choiceToRemove )) {
+      console.log("ERROR: Choice value doesn't exist!")
+    } else {
+      const index = this.#choices.indexOf(choiceToRemove);
+      this.#choices.slice(index, index);
+    }
+  }
+}
